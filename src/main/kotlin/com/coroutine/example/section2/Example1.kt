10e66d4dd54007b687c521bf171df2c658fe305b
ybchar <uiurihappy@naver.com>
2024-12-01 23:34:01 +0900

    Section2 코루틴 디버깅 및 CoroutineName

--- src/main/kotlin/com/coroutine/example/section2/Example1.kt
+++ src/main/kotlin/com/coroutine/example/section2/Example1.kt
@@ -1,15 +1,17 @@
 package com.coroutine.example.section2
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class Example1 {
 }
 
-fun main() = runBlocking<Unit> {
+fun main() = runBlocking<Unit>(CoroutineName("runBlocking 코루틴")) {
     println("[${Thread.currentThread().name}] runBlocking 코루틴 실행")
 
-    launch {
+    launch(CoroutineName("launch 코루틴")) {
         println("[${Thread.currentThread().name}] launch 코루틴 실행")
     }
 }
+// -Dkotlinx.coroutines.debug 로 VM 옵션을 주면 디버깅 모드로 실행됨
