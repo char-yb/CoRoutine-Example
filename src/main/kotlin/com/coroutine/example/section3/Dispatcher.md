52d3b260bbd256150449c1fed6a134b827782f46
ybchar <uiurihappy@naver.com>
2024-12-03 23:46:18 +0900

    Section3 CoroutineDispatcher IO, Default Example

--- src/main/kotlin/com/coroutine/example/section3/Dispatcher.md
+++ src/main/kotlin/com/coroutine/example/section3/Dispatcher.md
@@ -10,3 +10,6 @@ Dispatcher
 ### Dispatcher.Default
 CPU 바운드 작업을 위한 디스패처
 * CPU 바운드 작업: 이미지, 동영상 처리나 대용량 데이터 변환같은 끊이지 않고 연산이 필요한 작업.
+
+만약 모든 Thread가 이미지 처리 작업을 위해 투입이 된다면 동영상 처리나 대용량 데이터 변환같은 작업은 대기해야 한다.
+이런 경우에는 Dispatcher.Default를 사용하면 된다, limited Parallelism을 제공해 특정 작업 위에 사용할 수 있는 스레드의 수를 제한한다.
--- src/main/kotlin/com/coroutine/example/section3/Example3.kt
+++ src/main/kotlin/com/coroutine/example/section3/Example3.kt
@@ -8,6 +8,27 @@ class Example3 {
 }
 
 fun main() = runBlocking<Unit> {
+    val imageProcessingDispatcher = Dispatchers.Default.limitedParallelism(2)
+//    // Default
+//    repeat(100) {
+//        launch(imageProcessingDispatcher) {
+//            Thread.sleep(1000)
+//            println("[${Thread.currentThread().name}] 이미지 처리 완료")
+//        }
+//    }
+
+
+//    // IO
+//    // 다른 작업에 방해받지 않아야 하는 중요 작업이 있을 경우 사용
+//    val dedicatedDispatcher = Dispatchers.IO.limitedParallelism(2)
+//    repeat(100) {
+//        launch(dedicatedDispatcher) {
+//            Thread.sleep(1000)
+//            println("[${Thread.currentThread().name}] 작업 실행")
+//        }
+//    }
+
+
     launch(Dispatchers.IO) {
 //    launch(Dispatchers.Default) {
         launch {
