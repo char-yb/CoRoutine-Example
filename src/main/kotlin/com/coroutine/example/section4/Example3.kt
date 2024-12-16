1a76123f21d0381a73fc03e98790021690e6aab8
ybchar <uiurihappy@naver.com>
2024-12-16 13:43:46 +0900

    Section4 지연 코루틴

---
+++ src/main/kotlin/com/coroutine/example/section4/Example3.kt
@@ -0,0 +1,17 @@
+package com.coroutine.example.section4
+
+import kotlinx.coroutines.*
+
+class Example3 {
+}
+
+fun main() = runBlocking<Unit> {
+    val startTime = System.currentTimeMillis()
+    // 실행 요청되는 부분이 없기에 코루틴이 실행되지 않고 생성 상태에 있다.
+    val lazyJob: Job = launch(start = CoroutineStart.LAZY) {
+        println("[${getElapsedTime(startTime)}] launch 코루틴 지연 실행")
+    } // 출력 없음
+
+    delay(3000L)
+    lazyJob.start() // 코루틴 실행 join() 함수를 사용해도 무방
+}
